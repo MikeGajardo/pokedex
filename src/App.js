47197b4a119af44577ec7f997e3874e0e2f8101d
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2023-01-28 14:45:55 -0500

    initial api set-up w axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState('')
+  const getPokemon = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    .then(res => {
+      console.log(res.data.sprites.front_default)
+      setPokemon(res.data.sprites.front_default)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
       <div className="gameboy">
         <div className="display">
         <div className="screen">
-          <div className="pokeone"></div>
+          <div className="pokeone" onClick={getPokemon}></div>
           <div className="poketwo"></div>
         </div>
         </div>
